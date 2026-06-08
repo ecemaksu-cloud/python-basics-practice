01f57584496012ec52f0a335d5d099f09c2bf360
Ecem Aksu <ecem.aksu32@gmail.com>
2026-06-08 20:13:21 +0300

    Add simple HTTP server example

---
+++ 15_web_development/simple_http_server.py
@@ -0,0 +1,17 @@
+# Simple HTTP Server using Python
+
+import http.server
+import socketserver
+
+PORT = 8000
+
+Handler = http.server.SimpleHTTPRequestHandler
+
+httpd = socketserver.TCPServer(
+    ("", PORT),
+    Handler
+)
+
+print(f"Serving at port {PORT}")
+
+httpd.serve_forever()
