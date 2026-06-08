574676c18955f0306136f5cefabc83ad65976c2b
Ecem Aksu <ecem.aksu32@gmail.com>
2026-06-08 19:34:42 +0300

    Add addition function example

---
+++ 08_functions/addition_function.py
@@ -0,0 +1,9 @@
+# Add two numbers using a function
+
+def add_numbers(num1, num2):
+    return num1 + num2
+
+first_number = int(input("Enter the first number: "))
+second_number = int(input("Enter the second number: "))
+
+print("Sum =", add_numbers(first_number, second_number))
