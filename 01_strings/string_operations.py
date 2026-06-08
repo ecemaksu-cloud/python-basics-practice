5e8508621c328fdaea9e2b0c71f5bcf82ed2ce62
Ecem Aksu <ecem.aksu32@gmail.com>
2026-06-08 18:59:45 +0300

    Add string operations practice

---
+++ 01_strings/string_operations.py
@@ -0,0 +1,36 @@
+# Character replacement
+
+word = "Galata"
+word = "S" + word[1:]
+print(word)
+
+# String slicing
+
+place = "istanbul"
+print(place[0::2])
+print(place[1::4])
+
+# Splitting a word
+
+team = "GALATASARAY"
+
+word1 = team[0:6]
+word2 = team[6:11]
+
+print(word1)
+print(word2)
+
+# Reverse string
+
+name = "kaan"
+print(name[::-1])
+
+# Type conversion
+
+z = "8.77235641364654"
+
+a = float(z)
+b = int(a)
+
+print(a)
+print(b)
