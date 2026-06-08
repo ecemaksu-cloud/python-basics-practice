938af452c3f1c9b940a6a59752e53756e0c39eab
Ecem Aksu <ecem.aksu32@gmail.com>
2026-06-08 19:30:55 +0300

    Add power calculator using math module

---
+++ 07_modules/power_calculator.py
@@ -0,0 +1,8 @@
+# Calculate the power of a number using math.pow()
+
+import math
+
+base = int(input("Enter the base number: "))
+exponent = int(input("Enter the exponent: "))
+
+print(math.pow(base, exponent))
